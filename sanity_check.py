010b7518f4449e80dfb124efa59088951638560e
Sose1 <redstar0bluestar@gmail.com>
2023-10-02 22:43:54 +0900

    refact sanity_check

--- sanity_check.py
+++ sanity_check.py
@@ -4,73 +4,10 @@ import glob
 import os
 import numpy as np
 import pyperclip
+import pathlib
 
-def candidate_is_not_same():
-
-    target_folder = "training_add"
-    hcl = 5
-    files = sorted(glob.glob(target_folder+"/*.json"))
-    for file_num, file in enumerate(files):
-
-        # if os.path.exists(file.removesuffix(".json") + " (1).json"):
-        #     print("exists")
-        #     os.remove(file)
-        #     os.rename(file.removesuffix(".json") + " (1).json", file)
-        #     pass
-
-        with open(file, "r") as f:
-            task = json.load(f)
-        
-        test_tasks = [task["test"][idx*hcl:(idx+1)*hcl] for idx in range(int(len(task["test"])/hcl))] 
-        for test_task in test_tasks:
-            #collect candidates
-            candidates = []
-            true_answer = test_task[0]["output"]
-            candidates.append(true_answer)
-            for i in range(1, len(test_task)):
-                candidates.append(test_task[i]["input"])
-                candidates.append(test_task[i]["output"])
-
-            #Ensure that the candidates are not the same as each other.
-            for i, candidate in enumerate(candidates):
-                for j, candidate2 in enumerate(candidates):
-                    if i != j:#i == j -> same candidate
-                        candidate = np.array(candidate)
-                        candidate2 = np.array(candidate2)
-                        if candidate.shape == candidate2.shape:
-                            # print(np.all(candidate == candidate2))
-                            if np.all(candidate == candidate2):
-                                pyperclip.copy(file.removeprefix(target_folder+"\\"))
-                                print(file.removeprefix(target_folder+"\\"))
-                            
-                            assert not np.all(candidate == candidate2), "file num: {}\n{} candidate{} and candidata{} is same\n{}\n\n{}".format(file_num, file, i, j,candidate, candidate2)
-
-candidate_is_not_same()
 #%%
-def correction_add_task_len_more_than_two():
-    add_files = sorted(glob.glob("training_add_8/*.json"))
-    for file in add_files:
-        with open(file, "r") as f:
-            add_task = dict(sorted(json.load(f).items()))
-        if len(add_task["test"]) > 9:
-            print("a")
-            for in_out in add_task["test"]:
-                print(in_out["id"])
-            id_index = add_task["test"][0]["id"]
-            for i in range(len(add_task["test"])):
-                add_task["test"][i]["id"] = id_index
-                id_index += 1
-            print(add_task["test"])
-            # for in_out in add_task["test"]:
-            #     print(in_out["id"])
-
-            with open(file, "w") as f:
-                json.dump(add_task, f, separators=(",", ":"))
-
-correction_add_task_len_more_than_two()
-#%%
-import difflib
-def test_check_original_n_add_are_equal():
+def test_original_add_are_equal():
     def check_func(original_dir, add_dir):
 
         original_files = sorted(glob.glob(original_dir+"/*.json"))
@@ -103,32 +40,91 @@ def test_check_original_n_add_are_equal():
     check_func(original_dir="training_expand", add_dir="training_add_8")
 
 
-test_check_original_n_add_are_equal()
+test_original_add_are_equal()
 
 #%%
-def move_training_add_8():
-    target_folder = "training_add"
-    files = sorted(glob.glob(target_folder+"/*.json"))
-    for file in files:
+def candidate_is_not_same():
+
+    target_folder = pathlib.Path("training_add")
+    hcl = 5
+    files = sorted(glob.glob(str(target_folder/"*.json")))
+    for file_num, file in enumerate(files):
+        file_path = pathlib.Path(file)
 
         with open(file, "r") as f:
             task = json.load(f)
+        
+        test_tasks = [task["test"][idx*hcl:(idx+1)*hcl] for idx in range(int(len(task["test"])/hcl))] 
+        for test_task in test_tasks:
+            #collect candidates
+            candidates = []
+            true_answer = test_task[0]["output"]
+            candidates.append(true_answer)
+            for i in range(1, len(test_task)):
+                candidates.append(test_task[i]["input"])
+                candidates.append(test_task[i]["output"])
 
-        # print(len(task["test"]))
-        # print(len(task["test"][:5]))
+            #Ensure that the candidates are not the same as each other.
+            for i, candidate in enumerate(candidates):
+                for j, candidate2 in enumerate(candidates):
+                    if i != j:#i == j -> same candidate
+                        candidate = np.array(candidate)
+                        candidate2 = np.array(candidate2)
+                        if candidate.shape == candidate2.shape:
+                            if np.all(candidate == candidate2):
+                                pyperclip.copy(file_path.name)
+                                print(file_path.name)
+                            
+                            assert not np.all(candidate == candidate2), "file num: {}\n{} candidate{} and candidata{} is same\n{}\n\n{}".format(file_num, file, i, j,candidate, candidate2)
 
-        original_task_len = int(len(task["test"])/5)
-        if original_task_len != 1:
-            print(original_task_len)
-        for i in range(original_task_len):
-            task["test"].pop((i+1)*5-i)
+candidate_is_not_same()
+#%%
+# def move_training_add_8():
+#     target_folder = "training_add"
+#     files = sorted(glob.glob(target_folder+"/*.json"))
+#     for file in files:
 
-        # print("training_add_8"+file.removeprefix(target_folder))
-        with open("training_add_8"+file.removeprefix(target_folder), 'w') as f:
-            json.dump(task, f, separators=(',', ':'))
+#         with open(file, "r") as f:
+#             task = json.load(f)
+
+#         # print(len(task["test"]))
+#         # print(len(task["test"][:5]))
+
+#         original_task_len = int(len(task["test"])/5)
+#         if original_task_len != 1:
+#             print(original_task_len)
+#         for i in range(original_task_len):
+#             task["test"].pop((i+1)*5-i)
+
+#         # print("training_add_8"+file.removeprefix(target_folder))
+#         with open("training_add_8"+file.removeprefix(target_folder), 'w') as f:
+#             json.dump(task, f, separators=(',', ':'))
     
         
-    #     test_tasks = [task["test"][idx*hcl:(idx+1)*hcl] for idx in range(int(len(task["test"])/hcl))] 
-    # print(original_files[0])
+#     #     test_tasks = [task["test"][idx*hcl:(idx+1)*hcl] for idx in range(int(len(task["test"])/hcl))] 
+#     # print(original_files[0])
 
-move_training_add_8()
+# move_training_add_8()
+
+# #%%
+# def correction_add_task_len_more_than_two():
+#     add_files = sorted(glob.glob("training_add_8/*.json"))
+#     for file in add_files:
+#         with open(file, "r") as f:
+#             add_task = dict(sorted(json.load(f).items()))
+#         if len(add_task["test"]) > 9:
+#             print("a")
+#             for in_out in add_task["test"]:
+#                 print(in_out["id"])
+#             id_index = add_task["test"][0]["id"]
+#             for i in range(len(add_task["test"])):
+#                 add_task["test"][i]["id"] = id_index
+#                 id_index += 1
+#             print(add_task["test"])
+#             # for in_out in add_task["test"]:
+#             #     print(in_out["id"])
+
+#             with open(file, "w") as f:
+#                 json.dump(add_task, f, separators=(",", ":"))
+
+# correction_add_task_len_more_than_two()
